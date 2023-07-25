4ec5dcc0e2e91c7ef02c02b4a0a6647ca56ff475
Steve Martin <Stevemartin677@gmail.com.com>
2023-07-25 04:01:03 -0600

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import from react-dom/client
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
