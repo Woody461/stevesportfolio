04cb3609b09c3493f8ec583e69855c46f5176bde
Steve Martin <Stevemartin677@gmail.com.com>
2023-07-25 04:08:54 -0600

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StrictMode } from 'react'; // Import StrictMode
-
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rootElement = document.getElementById('root');
-
-// Use createRoot instead of ReactDOM.render
-ReactDOM.createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
